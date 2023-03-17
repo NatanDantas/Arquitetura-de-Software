c777d0a7cb42ac88702507dd1213b4565c45b332
Natan Dantas <jsda201314@gmail.com>
2023-03-17 03:11:18 -0300

    Alterações na tela e lógica de cadastro

--- src/php/Cadastro.php
+++ src/php/Cadastro.php
@@ -4,35 +4,29 @@ session_start();
 include("conexao.php");
 $nome = mysqli_real_escape_string($mysqli, $_POST['nome']);
 $Cpf = mysqli_real_escape_string($mysqli, $_POST['Cpf']);
-$DDD = mysqli_real_escape_string($mysqli, $_POST['DDD']);
 $Email = mysqli_real_escape_string($mysqli, $_POST['Email']);
 $Senha = mysqli_real_escape_string($mysqli, trim(md5($_POST['Senha'])));
-$grupos = mysqli_real_escape_string($mysqli, ($_POST['grupos']));
-
-$upload = salvarFtperfil($_FILES);
-if(gettype($upload) == "string"){
-	$File = $upload;
-}
+$Grupos = mysqli_real_escape_string($mysqli, ($_POST['Grupos']));
+$Login = mysqli_real_escape_string($mysqli, $_POST['Login']);
 
 $sql = "select count(*) as total from Usuario where login = '$Login'";
 $result = $mysqli->query($sql);
 $row = mysqli_fetch_assoc($result);
 
-if($row['total'] == 1) {
+if ($row['total'] == 1) {
 	$_SESSION['usuario_existe'] = true;
 	header('Location: Cadastro.html');
 	exit;
 }
 
-$sql = "INSERT INTO Usuario(nmUsuario, cpf, dtNascimento, email, endereco, numero, cep, login, senha, ftPerfil, descricao,cargo,ddd,telefone)
-        VALUES('$nome','$Cpf', '$DatadeNascimento', '$Email', '$Endereco', '$Numero', 
-        '$Cep', '$Login', '$Senha', '$File', '$Descricao', '$Cargo', '$DDD', '$Telefone')";
+$sql = "INSERT INTO Usuario(nmUsuario, cpf, email, login, senha,grupos)
+        VALUES('$nome','$Cpf','$Email','$Login', '$Senha', '$Grupos')";
 
-if($mysqli->query($sql) === TRUE) {
+if ($mysqli->query($sql) === TRUE) {
 	$_SESSION['status_cadastro'] = true;
 	header('Location: Login.html');
 	exit;
-}else{
+} else {
 	echo $mysqli->error;
 }
 
